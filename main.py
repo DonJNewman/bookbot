1ec3233d8e176956a863f4177527b955741d6b4f
DonJNewman <Dnewman1934@yahoo.com>
2025-03-15 17:51:42 -0500

    begin of char counting function

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from stats import text_split
 def get_book_text(filepath):
     with open(filepath) as f: 
         file_contents = f.read()
@@ -9,17 +10,4 @@ def main():
     text_split(
         get_book_text("books/frankenstein.txt")
         )
-
-def text_split(filestring):
-    text_word_count=len(filestring.split())
-    print(f"{text_word_count} words found in the document")
-    return text_word_count
-
-
 main()
-
-
-
-# text split function, takes file_contents and returns number of words
-#split method will turn into list, return length of list
-# print {num_words} words found in the document
---
+++ stats.py
@@ -0,0 +1,4 @@
+def text_split(filestring):
+    text_word_count=len(filestring.split())
+    print(f"{text_word_count} words found in the document")
+    return text_word_count
