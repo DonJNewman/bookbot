f244e0c5bf7c32a6e42c6221fae45dd558e68b88
DonJNewman <Dnewman1934@yahoo.com>
2025-03-15 17:47:09 -0500

    successful word count return

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
-
-
-
 def get_book_text(filepath):
-
-    with open(filepath) as f:
+    with open(filepath) as f: 
         file_contents = f.read()
-        print('hey_')
-        print(file_contents)
-
+        print('file contents read successfully')
     return file_contents
 
-
 def main(): 
-    get_book_text("books/frankenstein.txt")
+   
+    text_split(
+        get_book_text("books/frankenstein.txt")
+        )
+
+def text_split(filestring):
+    text_word_count=len(filestring.split())
+    print(f"{text_word_count} words found in the document")
+    return text_word_count
+
 
 main()
+
+
+
+# text split function, takes file_contents and returns number of words
+#split method will turn into list, return length of list
+# print {num_words} words found in the document
