ee857574f96048637bd99f17e1b061c0d8f23a64
DonJNewman <Dnewman1934@yahoo.com>
2025-03-15 19:55:48 -0500

    successful reporting of reversed sorted list of dictionaries

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import text_split, char_counter
+from stats import text_split, char_counter, sort_list, print_report
 def get_book_text(filepath):
     with open(filepath) as f: 
         file_contents = f.read()
@@ -7,7 +7,7 @@ def get_book_text(filepath):
 def main(): 
     gotten_book = get_book_text("books/frankenstein.txt")
     counted_characters = char_counter(gotten_book)
-
-    text_split(gotten_book)
-    print(counted_characters)
+    split_text = text_split(gotten_book)
+    sorted_list = sort_list(counted_characters)
+    print_report(sorted_list, split_text)
 main()
--- stats.py
+++ stats.py
@@ -10,5 +10,30 @@ def char_counter(filestring):
         if not lowercase in char_dict:
             char_dict[lowercase] = 1
         else:
-             char_dict[lowercase] += 1    
-    return char_dict 
+             char_dict[lowercase] += 1
+    return char_dict
+
+def sort_on(dictionary_list):
+    return dictionary_list["count"]
+
+def sort_list(char_dict):
+
+    list_of_dicts=[]
+    for key, value in char_dict.items():
+        list_of_dicts.append({"char" :key, "count":value})
+    list_of_dicts.sort(reverse=True, key=sort_on)
+    return list_of_dicts
+
+def print_report(sorted_list, wordcount):
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print('----------- Word Count ----------')
+    print(f"Found {wordcount} total words")
+    print("-------------Character Count----------")
+    for item in sorted_list:
+        char = item["char"]
+        count = item["count"]
+        if char.isalpha():
+            print(f"{char}: {count}:")
+    print("============= END ===============")
+    return None
