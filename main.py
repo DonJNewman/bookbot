e0864b2b78dfb0bead0b791a2f7c5548d8420c57
DonJNewman <Dnewman1934@yahoo.com>
2025-03-15 20:10:10 -0500

    Removal of hardcoded values, now usable by any with directions how to input the target

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 from stats import text_split, char_counter, sort_list, print_report
+import sys
 def get_book_text(filepath):
     with open(filepath) as f: 
         file_contents = f.read()
     return file_contents
 
-def main(): 
-    gotten_book = get_book_text("books/frankenstein.txt")
-    counted_characters = char_counter(gotten_book)
-    split_text = text_split(gotten_book)
-    sorted_list = sort_list(counted_characters)
-    print_report(sorted_list, split_text)
+def main():
+    print(len(sys.argv))
+    if len(sys.argv) == 1:
+        print('Usage: python3 main.py <path_to_book>')
+        sys.exit(1)
+    else:
+        gotten_book = get_book_text(sys.argv[1])
+        counted_characters = char_counter(gotten_book)
+        split_text = text_split(gotten_book)
+        sorted_list = sort_list(counted_characters)
+        print_report(sorted_list, split_text)
 main()
--- stats.py
+++ stats.py
@@ -1,6 +1,5 @@
 def text_split(filestring):
     text_word_count=len(filestring.split())
-    print(f"{text_word_count} words found in the document")
     return text_word_count
 
 def char_counter(filestring):
